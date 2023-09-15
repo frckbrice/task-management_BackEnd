948fbaad98db8baedc7493a50e868f98ffe39763
frckbrice <maebrie2017@gmail.com>
2023-09-15 13:55:47 +0100

    fixing the jwtstrategy

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,33 @@
+//*local connection
+// module.exports = {
+//   DB: "tmsDB",
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "Password123#@!",
+//   DIALECT: "mysql",
+// };
+
+//* db4free connection
 module.exports = {
-  DB: "tmsDB",
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "Password123#@!",
+  DB: "tmsgdb2",
+  HOST: "db4free.net",
+  USER: "tmsgdb2",
+  PASSWORD: "test0123",
   DIALECT: "mysql",
+  PORT: 3306,
 };
+
+//* entring connection fromm db4free
+// const mysql = require('mysql');
+
+// const pool = mysql.createPool({
+//   host: 'db4free.net',
+//   port: 3306,
+//   user: 'tmsgdb2',
+//   password: 'test0123',
+//   database: 'tmsgdb2',
+//   connectionLimit: 100,
+//   multipleStatements: true,
+// })
+
+// module.exports = pool;
