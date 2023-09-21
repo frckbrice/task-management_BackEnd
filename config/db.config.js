123633e2ca89dbde3dddc9b0d3e2a1446e13abc9
frckbrice <maebrie2017@gmail.com>
2023-09-21 15:24:51 +0100

    get all project from connected user

--- config/db.config.js
+++ config/db.config.js
@@ -1,21 +1,21 @@
 
-// module.exports = {
-//   DB: "tmsDB",
-//   HOST: "localhost",
-//   USER: "root",
-//   PASSWORD: "Password123#@!",
-//   DIALECT: "mysql",
-// };
+module.exports = {
+  DB: "tmsDB",
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "Password123#@!",
+  DIALECT: "mysql",
+};
 
 //* db4free connection
-module.exports = {
-  DB: "tmsgdb2",
-  HOST: "db4free.net",
-  USER: "tmsgdb2",
-  PASSWORD: "test0123",
-  DIALECT: "mysql",
-  PORT: 3306,
-};
+// module.exports = {
+//   DB: "tmsgdb2",
+//   HOST: "db4free.net",
+//   USER: "tmsgdb2",
+//   PASSWORD: "test0123",
+//   DIALECT: "mysql",
+//   PORT: 3306,
+// };
 
 //* entring connection fromm db4free
 // const mysql = require('mysql');
