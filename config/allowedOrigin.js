8575db973ed307b203cc05de82a1899a60641cd3
frckbrice <maebrie2017@gmail.com>
2023-09-28 14:29:20 +0100

    updating task status

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -3,5 +3,6 @@ module.exports = {
     "http://localhost:3000",
     "https://frontend-tasktrec-5hsncdtd8-nsamedaisy.vercel.app",
     "https://task-management-system-frontend.vercel.app",
+    '*'
   ],
 };
