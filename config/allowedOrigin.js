a311c007dac516cf5e9e466d4d6e2aa95ed5b684
frckbrice <maebrie2017@gmail.com>
2023-11-22 23:40:03 +0100

    modify the allow origin to take the new vercel url

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -2,10 +2,6 @@ module.exports = {
   allowedOrigin: [
     "http://localhost:3000",
     "http://localhost:3001",
-    "https://frontend-tasktrec-5hsncdtd8-nsamedaisy.vercel.app",
-    "https://task-management-system-frontend.vercel.app",
-    "https://task-management-system-frontend-7p16aywpz-frckbrice.vercel.app",
-    "task-management-system-frontend.vercel.app",
-    "https://task-management-system-frontend-git-feature-fu-d23a45-frckbrice.vercel.app/",
+    "https://https://tasktrecmanagement.vercel.app/",
   ],
 };
