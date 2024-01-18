972281855170406ddb9797dd8961e8fcf18e8d2d
frckbrice <maebrie2017@gmail.com>
2024-01-18 20:35:34 +0100

    change the original link from remote repo

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,7 +1,8 @@
 module.exports = {
   allowedOrigin: [
-    "https://tms-app-frontend.vercel.app/",
+    "https://tms-app-frontend.vercel.app",
     "http://localhost:3000",
     "http://localhost:3001",
   ],
 };
+// ("https://tms-app-frontend.vercel.app");
